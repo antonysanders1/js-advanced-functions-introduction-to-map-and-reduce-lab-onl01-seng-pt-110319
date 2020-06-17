b7a66687e1ec4409d29471ebbd94bf3658487ae9
antony_sanders <antony.devon92@gmail.com>
2020-06-17 03:23:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
-// Your code here
+// ---- Mapping Practice
+function mapToNegativize(numbers) {
+    let newArr = []
+    numbers.map(number => {
+        newArr.push(number * -1);
+    })
+    return newArr;
+}
+
+function mapToNoChange(numbers) {
+    let newArr = []
+    numbers.map(number => {
+        newArr.push(number);
+    })
+    return newArr;
+}
+
+function mapToDouble(numbers) {
+    let newArr = []
+    numbers.map(number => {
+        newArr.push(number * 2);
+    })
+    return newArr;
+}
+
+function mapToSquare(numbers) {
+    let newArr = []
+    numbers.map(number => {
+        newArr.push(number ** 2);
+    })
+    return newArr;
+}
+
+
+// ---- Reducing Practice
+// working on this method below...
+// function reduceToTotal(sourceArr, startingPoint = 0) {
+//     let sum = sourceArr.reduce((number, startingPoint) => startingPoint + number);
+//     return sum;
+// }
+
+//alternative solution the above method
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    let total = startingPoint;
+    for (let i = 0; i < sourceArray.length; i++) {
+        total += sourceArray[i];
+    };
+    return total;
+};
+
+function reduceToAllTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (!sourceArray[i])
+            return false
+    };
+    return true;
+};
+
+function reduceToAnyTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (sourceArray[i])
+            return true
+    };
+    return false;
+};
